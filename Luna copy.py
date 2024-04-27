429255be77382f3d368f742d4589bb960be1cd11
Saravanan <94352332+Saravanan-e-2003@users.noreply.github.com>
2024-04-27 12:21:05 +0530

    Update Luna copy.py

--- Luna copy.py
+++ Luna copy.py
@@ -1,44 +1,43 @@
 import pyautogui
 import random
 import tkinter as tk
-
+from PIL import Image,ImageTk
 x = 1000
 cycle = 0
 check = 1
 idle_num =[1,2,3,4]
-play_num = [10,11,12,13,15]
+sleep_num = [10,11,12,13,15]
 walk_left = [6,7]
 walk_right = [8,9]
-jump_num = [16,17,18]
 event_number = random.randrange(1,3,1)
-impath = 'Assets/'
-
+impath = 'Assets\\'
 #transfer random no. to event
 def event(cycle,check,event_number,x):
     if event_number in idle_num:
         check = 0
+        #print('idle')
         window.after(100,update,cycle,check,event_number,x) #no. 1,2,3,4 = idle
     elif event_number == 5:
         check = 1
+        #print('from idle to sleep')
         window.after(100,update,cycle,check,event_number,x) #no. 5 = idle to sleep
     elif event_number in walk_left:
         check = 4
+        #print('walking towards left')
         window.after(100,update,cycle,check,event_number,x)#no. 6,7 = walk towards left
     elif event_number in walk_right:
         check = 5
+        #print('walking towards right')
         window.after(100,update,cycle,check,event_number,x)#no 8,9 = walk towards right
-    elif event_number in jump_num:
-        check  = 6
-        print("jump")
-        window.after(100,update,cycle,check,event_number,x)#no. 16,17 = sleep
-    elif event_number in play_num:
+    elif event_number in sleep_num:
         check  = 2
-        window.after(100,update,cycle,check,event_number,x)#no. 10,11,12,13,15 = play
+        #print('sleep')
+        window.after(100,update,cycle,check,event_number,x)#no. 10,11,12,13,15 = sleep
     elif event_number == 14:
         check = 3
+        #print('from sleep to idle')
         window.after(100,update,cycle,check,event_number,x)#no. 15 = sleep to idle
-
-#looping through gif frames
+#making gif work 
 def gif_work(cycle,frames,event_number,first_num,last_num):
     if cycle < len(frames) -1:
         cycle+=1
@@ -48,52 +47,50 @@ def gif_work(cycle,frames,event_number,first_num,last_num):
     return cycle,event_number
 
 def update(cycle,check,event_number,x):
-
+    #idle
     if check ==0:
         frame = idle[cycle]
         cycle ,event_number = gif_work(cycle,idle,event_number,1,9)
+
+    #idle to sleep
     elif check ==1:
-        frame = idle_to_lick[cycle]
-        cycle ,event_number = gif_work(cycle,idle_to_lick,event_number,10,10)
+        frame = idle_to_sleep[cycle]
+        cycle ,event_number = gif_work(cycle,idle_to_sleep,event_number,10,10)
+#sleep
     elif check == 2:
-        frame = play[cycle]
-        cycle ,event_number = gif_work(cycle,play,event_number,10,15)
+        frame = sleep[cycle]
+        cycle ,event_number = gif_work(cycle,sleep,event_number,10,15)
+#sleep to idle
     elif check ==3:
-        frame = play_to_lick[cycle]
-        cycle ,event_number = gif_work(cycle,play_to_lick,event_number,1,1)
+        frame = sleep_to_idle[cycle]
+        cycle ,event_number = gif_work(cycle,sleep_to_idle,event_number,1,1)
+#walk toward left
     elif check == 4:
         frame = walk_positive[cycle]
         cycle , event_number = gif_work(cycle,walk_positive,event_number,1,9)
         x -= 10
+#walk towards right
     elif check == 5:
         frame = walk_negative[cycle]
         cycle , event_number = gif_work(cycle,walk_negative,event_number,1,9)
         x -= -10
-    elif check == 6:
-        frame = jump[cycle]
-        cycle , event_number = gif_work(cycle,jump,event_number,15,18)
-        print("atUpdate")
     window.geometry('100x100+'+str(x)+'-57')
     label.configure(image=frame)
     window.after(1,event,cycle,check,event_number,x)
-
 window = tk.Tk()
-
-#Assets calling
+#call buddy's action gif
 idle = [tk.PhotoImage(file=impath+'idle.gif',format = 'gif -index %i' %(i)) for i in range(8)]#idle gif
-idle_to_lick = [tk.PhotoImage(file=impath+'lick.gif',format = 'gif -index %i' %(i)) for i in range(8)]#idle to sleep gif
-play = [tk.PhotoImage(file=impath+'play.gif',format = 'gif -index %i' %(i)) for i in range(3)]#sleep gif
-play_to_lick = [tk.PhotoImage(file=impath+'lick.gif',format = 'gif -index %i' %(i)) for i in range(8)]#sleep to idle gif
+idle_to_sleep = [tk.PhotoImage(file=impath+'play.gif',format = 'gif -index %i' %(i)) for i in range(6)]#idle to sleep gif
+sleep = [tk.PhotoImage(file=impath+'lick.gif',format = 'gif -index %i' %(i)) for i in range(8)]#sleep gif
+sleep_to_idle = [tk.PhotoImage(file=impath+'idle.gif',format = 'gif -index %i' %(i)) for i in range(8)]#sleep to idle gif
 walk_positive = [tk.PhotoImage(file=impath+'run_negative.gif',format = 'gif -index %i' %(i)) for i in range(8)]#walk to left gif
 walk_negative = [tk.PhotoImage(file=impath+'run.gif',format = 'gif -index %i' %(i)) for i in range(8)]#walk to right gif
-jump = [tk.PhotoImage(file=impath+'jump.gif',format = 'gif -index %i' %(i)) for i in range(6)]
-
-window.config(highlightbackground='white')
-label = tk.Label(window,bd=0,bg='white')
+#window configuration
+window.config(highlightbackground='black')
+label = tk.Label(window,bd=0,bg='black')
 window.overrideredirect(True)
-window.wm_attributes('-transparentcolor','white')
+window.wm_attributes('-transparentcolor','black')
 label.pack()
-
-#main event loop
-window.after(1,update,cycle,check,event_number,x)
+#loop the program
+window.after(3,update,cycle,check,event_number,x)
 window.mainloop()
